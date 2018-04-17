1ec4d37557610ac44fa5a6f150a827ee4c4f5252
Alexander Backenhof <alexander.backenhof@sony.com>
2018-04-17 07:40:53 +0200

    Fireworks initial upload

---
+++ app/src/main/cpp/fireworks/engine.cpp
@@ -0,0 +1,14 @@
+#include "engine.h"
+
+namespace Fireworks {
+
+    bool Engine::init(AAssetManager* pAssetManager)
+    {
+        return true;
+    }
+
+    bool Engine::render()
+    {
+        return true;
+    }
+}
